ab3ad5bcede7b0fd5c069e77b074d8b832bb9b91
Mo'men M <moamenng@gmail.com>
2020-09-22 19:12:24 +0200

    commit

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,47 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Bro <3",
+      message: "You are so amazing !",
+      time: "15:44",
+      avatarUrl:
+          "https://png.pngitem.com/pimgs/s/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"),
+  new ChatModel(
+      name: "Dad",
+      message: "Hello There!",
+      time: "17:30",
+      avatarUrl:
+          "https://png.pngitem.com/pimgs/s/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"),
+  new ChatModel(
+      name: "Mike",
+      message: "Wassup !",
+      time: "6:00",
+      avatarUrl:
+          "https://png.pngitem.com/pimgs/s/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"),
+  new ChatModel(
+      name: "Rachel",
+      message: "I'm good!",
+      time: "10:30",
+      avatarUrl:
+          "https://png.pngitem.com/pimgs/s/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"),
+  new ChatModel(
+      name: "Allen",
+      message: "I'm alive!",
+      time: "13:30",
+      avatarUrl:
+          "https://png.pngitem.com/pimgs/s/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"),
+  new ChatModel(
+      name: "Joe",
+      message: "You are so cool !",
+      time: "15:30",
+      avatarUrl:
+          "https://png.pngitem.com/pimgs/s/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"),
+];
